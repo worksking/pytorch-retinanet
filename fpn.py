c49537cd9178d3ea4d3a65fd1b35da8f9aeec0b1
admin <13128965575@163.com>
2019-06-11 17:45:57 +0800

    fpn

--- fpn.py
+++ fpn.py
@@ -27,12 +27,15 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        identity = x
+        identity = x  ####
+
         out = F.relu(self.bn1(self.conv1(x)))
         out = F.relu(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
-        if self.downsample is not None:
+
+        if self.downsample is not None:    ####
             identity = self.downsample
+
         out += identity
         out = F.relu(out)
         return out
